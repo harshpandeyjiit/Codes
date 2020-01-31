9b61f38c88e7360adb8994e28cc5e77e7a304063
unknown <harshpandeyjiit@gmail.com>
2020-02-01 03:05:19 +0530

    UpdatedGraphQuestions

--- Graph Algortihms/RhezoMind.cpp
+++ Graph Algortihms/RhezoMind.cpp
@@ -42,6 +42,107 @@ typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_nod
 #define MAXN 10005
 int n,m,ans=0; // number of nodes
 vector<int> adj[MAXN]; // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children=0;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] >= tin[v] && p!=-1)
+                ans++;
+            ++children;
+        }
+    }
+    if(p == -1 && children > 1)ans++;
+}
+
+void find_cutpoints() {
+    timer = 0;
+    visited.assign(n+1, false);
+    tin.assign(n+1, -1);
+    low.assign(n+1, -1);
+    for (int i = 1; i <=n; ++i) {
+        if (!visited[i])
+            dfs (i);
+    }
+}
+
+int32_t main()
+{
+    fastio
+    int t=1;
+    //cin>>t;
+    while(1)
+    {
+        cin>>n>>m;
+        if(!n && !m)return 0;
+        rep(i,m)
+        {
+            int a,b;
+            cin>>a>>b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        find_cutpoints();
+        cout<<ans<<endl;
+        ans=0;
+    }
+    return 0;
+}
+/*#          #  # # # # #  # # # # #  # # # # #  #          #
+  #          #  #       #  #       #  #          #          #
+  # # # # # ##  # # # # #  ## # # #   # # # # #  # # # # # ##
+  #          #  #       #  #    #             #  #          #
+  #          #  #       #  #      #   # # # # #  #          #*/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define mkp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define debug1(a) cout<<a<<endl;
+#define debug2(a,b) cout<<a<<' '<<b<<endl;
+#define debug3(a,b,c) cout<<a<' '<<b<<' '<<c<<endl;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repr(i,a,b)for(int i=a;i<b;i++)
+#define repre(i,a,b)for(int i=a;i<=b;i++)
+#define pi pair<int,int>
+#define pii pair<int,pi>
+#define mp map<int,int>
+#define iofile freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define ump unordered_map<int,int>
+#define st set<int>
+#define ust unordered_set<int>
+#define mst multiset<int>
+#define pq priority_queue
+#define mpq priority_queue<int,vector<int>,greater<int> >
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define sumofdigits(sum,num) int sum = 0; while (n != 0) { sum = sum + n % 10; n = n/10; }
+#define int2str(str,n) n=boost::lexical_cast<int>(str);
+#define float2str(str,n) n=boost::lexical_cast<float>(str);
+using namespace __gnu_pbds;
+using namespace std;
+#define Endl endl
+#define ar(n) int arr[n]
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> orderedSet;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
+//*p.find_by_order(index) return value at index
+//p.order_of_key(key) return index
+#define MAXN 10005
+int n,m,ans=0; // number of nodes
+vector<int> adj[MAXN]; // adjacency list of graph
 int indegree[MAXN],artpt[MAXN];
 vector<bool> visited;
 vector<int> tin, low;
