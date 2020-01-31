b22a20e2f67cb8348c1d3aa2ae2d6a21912cdde7
unknown <harshpandeyjiit@gmail.com>
2020-01-31 19:25:59 +0530

    "Updated Graph Questions"

--- Graph Algortihms/LonelyIsland.cpp
+++ Graph Algortihms/LonelyIsland.cpp
@@ -1,9 +1,12 @@
+
 /*#          #  # # # # #  # # # # #  # # # # #  #          #
   #          #  #       #  #       #  #          #          #
   # # # # # ##  # # # # #  ## # # #   # # # # #  # # # # # ##
   #          #  #       #  #    #             #  #          #
   #          #  #       #  #      #   # # # # #  #          #*/
 #include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
 #define int long long
 #define mkp make_pair
 #define pb push_back
@@ -29,48 +32,68 @@
 #define sumofdigits(sum,num) int sum = 0; while (n != 0) { sum = sum + n % 10; n = n/10; }
 #define int2str(str,n) n=boost::lexical_cast<int>(str);
 #define float2str(str,n) n=boost::lexical_cast<float>(str);
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 using namespace std;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> orderedSet;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
+#define Endl endl
+#define ar(n) int arr[n]
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> orderedSet;
+// typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
 //*p.find_by_order(index) return value at index
 //p.order_of_key(key) return index
-#define maxn 1000004
-
-int BIT[maxn],n,q,z;
-
-void update(int x, int delta)
+vector<int>v[200005],ans;
+queue<int>t;
+#define eps 1e-9
+int n,m,r,in[200005];
+double val[200005],out[200005];
+void topological_sort()
 {
-      for(; x <= maxn; x += x&-x)BIT[x] += delta;
+	for(int i=1;i<=n;i++)
+	{
+		if(!in[i])
+		{
+			t.push(i);
+		}
+	}
+	while(!t.empty())
+	{
+		int u=t.front();
+		t.pop();
+		ans.push_back(u);
+		for(auto i:v[u])
+		{
+			in[i]-=1;
+			val[i]+=val[u]/out[u];
+			if(in[i]==0)
+			{
+				t.push(i);
+			}
+		}
+	}
 }
-
-int query(int x)
-{
-     int sum = 0;
-     for(; x > 0; x -= x&-x)sum += BIT[x];
-     return sum;
-}
-
 int32_t main()
 {
-    fastio
-    int t=1;
-    while(t--)
-    {
-		cin>>n>>q>>z;
-		rep(i,n)
+	cin>>n>>m>>r;
+	for(int i=1;i<=m;i++)
+	{
+		int x,y;
+		cin>>x>>y;
+		v[x].push_back(y);
+		out[x]+=1.0;
+		in[y]++;
+	}
+	val[r]=1;
+	topological_sort();
+	assert(ans.size()==n);
+	double maxx=0;
+	for(int i=1;i<=n;i++)
+		if(!out[i])
+			maxx=max(maxx,val[i]);
+	for(int i=1;i<=n;i++)
+	{
+		if(!out[i] && fabs(val[i]-maxx)<=eps)
 		{
-			int a,b;
-            cin>>a>>b;
-			update(a+1,1);
-            update(b+2,-1);
+			cout<<i<<" ";
 		}
-		rep(i,q)
-        {
-            int a;
-            cin>>a;
-            cout<<query(a+1)<<endl;
-        }
-    }
-    return 0;
+	}
+	return 0;
 }
--- Graph Algortihms/TOPOSORT.cpp
+++ Graph Algortihms/TOPOSORT.cpp
@@ -4,8 +4,8 @@
   #          #  #       #  #    #             #  #          #
   #          #  #       #  #      #   # # # # #  #          #*/
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
 #define int long long
 #define mkp make_pair
 #define pb push_back
@@ -31,12 +31,12 @@
 #define sumofdigits(sum,num) int sum = 0; while (n != 0) { sum = sum + n % 10; n = n/10; }
 #define int2str(str,n) n=boost::lexical_cast<int>(str);
 #define float2str(str,n) n=boost::lexical_cast<float>(str);
-using namespace __gnu_pbds;
+//using namespace __gnu_pbds;
 using namespace std;
 #define Endl endl
 #define ar(n) int arr[n]
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> orderedSet;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> orderedSet;
+// typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
 //*p.find_by_order(index) return value at index
 //p.order_of_key(key) return index
 int32_t main()
