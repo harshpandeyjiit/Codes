c84b93a31110317eaefc31b65ef5a8303fa21a02
unknown <harshpandeyjiit@gmail.com>
2020-02-22 17:20:27 +0530

    MST Questions Solved

--- Graph Algortihms/MARYBMW.cpp
+++ Graph Algortihms/MARYBMW.cpp
@@ -26,7 +26,7 @@
 #define ust unordered_set<int>
 #define mst multiset<int>
 #define pq priority_queue
-#define mpq priority_queue<pi,vector<pi>,greater<pi> >
+#define mpq priority_queue<int,vector<int>,greater<int> >
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define sumofdigits(sum,num) int sum = 0; while (n != 0) { sum = sum + n % 10; n = n/10; }
 #define int2str(str,n) n=boost::lexical_cast<int>(str);
@@ -39,52 +39,86 @@ typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_upda
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
 //*p.find_by_order(index) return value at index
 //p.order_of_key(key) return index
-#define MAXN 100005
-vector<pi> adjlist[MAXN];
-bool visited[MAXN]={0};
-int prims(int x)
+#define MAX 50005
+vector< pair<int,pair<int,int> > > v;
+int parent[MAX];
+int ranks[MAX],n;
+
+void __init__()
 {
-    int mincost=0;
-    mpq que;
-    pi p;
-    que.push(mkp(0,x));
-    while(!que.empty())
-    {
-        p=que.top();
-        que.pop();
-        int x=p.second;
-        if(visited[x])continue;
-        mincost+=p.first;
-        visited[x]=true;
-        for(int i=0;i<adjlist[x].size();i++)
-        {
-            int y=adjlist[x][i].second;
-            if(!visited[y])
-            {
-                que.push(adjlist[x][i]);
-            }
-        }
-    }
-    return mincost;
+	for(int i=1;i<=n;i++)
+	{
+		parent[i] = i;
+		ranks[i] =0;
+	}
+}
+int find(int x)
+{
+	while(x != parent[x])
+	{
+		parent[x] = parent[parent[x]];
+		x = parent[x];
+	}
+	return x;
+}
+
+void Union(int x1,int y1)
+{
+	if(ranks[x1] > ranks[y1])
+	{
+		parent[y1] = x1;
+		ranks[x1] = ranks[x1] + ranks[y1];
+	}
+	else
+	{
+		parent[x1] = y1;
+		ranks[y1] = ranks[y1] + ranks[x1];
+	}
+}
+
+bool ok()
+{
+	int x = find(1);
+	int y = find(n);
+	if(x==y)return true;
+	else return false;
+}
+
+bool check(int m)
+{
+	__init__();
+	for(int i=0;i<m;i++)
+	{
+		int x = find(v[i].second.first);
+		int y = find(v[i].second.second);
+		if(x==y)continue;
+		Union(x,y);
+		if(ok())
+		{
+			cout << v[i].first << endl;
+			return true;
+		}
+	}
+	return false;
 }
 
 int32_t main()
 {
     fastio
-    int tcase=1;
-    //cin>>tcase;
-    while(tcase--)
-    {
-        int n,m,x,y,weight;
-        cin>>n>>m;
-        while(m--)
-        {
-            cin>>x>>y>>weight;
-            --x,--y;
-            adjlist[x].push_back(mkp(weight,y));
-            adjlist[y].push_back(mkp(weight,x));
-        }
-        cout<<prims(0)<<endl;
-    }
-    return 0;
+	int t,m,a,b;
+	int c;
+	cin>>t;
+	while(t--)
+	{
+		v.clear();
+		cin>>n>>m;
+		for(int i=0;i<m;i++)
+		{
+			cin>>a>>b>>c;
+			v.pb(mkp(c,mkp(a,b)));
+		}
+		sort(v.rbegin(),v.rend());
+		if(!check(m))cout<<"-1"<<endl;
+	}
+	return 0;
 }
