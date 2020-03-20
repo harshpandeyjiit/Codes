f3977be4a9244f1b307709ae956018971180723c
harshpandeyjiit <harshpandeyjiit@gmail.com>
2020-03-20 20:33:15 +0530

    "Updated Graph Questions"

--- Graph Algortihms/temp.cpp
+++ Graph Algortihms/temp.cpp
@@ -1,8 +1,3 @@
-/*#          #  # # # # #  # # # # #  # # # # #  #          #
-  #          #  #       #  #       #  #          #          #
-  # # # # # ##  # # # # #  ## # # #   # # # # #  # # # # # ##
-  #          #  #       #  #    #             #  #          #
-  #          #  #       #  #      #   # # # # #  #          #*/
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -39,39 +34,78 @@ typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_upda
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> orderedMSet;
 //*p.find_by_order(index) return value at index
 //p.order_of_key(key) return index
+int gcd(int a, int b){return b ? gcd (b, a % b) : a;}
+int mpower(int x,int y,int p);
+const int maxn=1000009;
+int arr[maxn];
+const int maxm=10000009;
+int prime[maxm];
+int n;
+map<int,int>max_map;
+const int mod=1e9+7;
+
+void sieve()
+{
+  prime[0]=1;
+  prime[1]=1;
+  for(int i=2;i*i<=10000000;i++)
+  {
+    if(prime[i]==0)
+    {
+      prime[i]=i;
+      for(int j=i*i;j<=10000000;j+=i)
+      {
+        if(prime[j]==0)prime[j]=i;
+      }
+    }
+  }
+  for(int i=1000;i<=10000000;i++)
+  {
+    if(prime[i]==0)prime[i]=i;
+  }
+}
+
+int lcmMod()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int num = arr[i];
+        map<int, int> temp;
+        while (num > 1)
+        {
+            int factor = prime[num];
+            temp[factor]++;
+            num /= factor;
+        }
+        for (auto it : temp)max_map[it.ff] = max(max_map[it.ff], it.ss);
+    }
+    int ans = 1;
+    for (auto it : max_map)ans = ((ans%mod) * (mpower(it.ff, it.ss,mod)%mod)) % mod;
+    return ans%mod;
+}
 
 int32_t main()
 {
-    fastio
-    int tcase=1;
-    //cin>>tcase;
-    while(tcase--)
-    {
-        string str;
-    	cin>>str;
-    	vector<int> ans;
-    	int ind=0;
-    	repr(i,ind,str.length())
-    	{
-    		if(str[i]!='(')continue;
-    		for(int j=str.length()-1;j>i;j--)
-    		{
-    			if(str[j]==')')
-    			{
-    				ans.push_back(i);
-    				ans.push_back(j);
-    				str[j]='*';
-    				i=j;
-    			}
-    		}
-    	}
-    	if(ans.size()==0)
-    	{
-    		cout<<0<<Endl;
-    		return 0;
-    	}
-    	cout<<1<<endl<<ans.size()<<endl;
-    	rep(i,ans.size())cout<<ans[i]+1<<" ";
+  fastio
+  sieve();
+  cin>>n;
+  repre(i,1,n)cin>>arr[i];
+  int k;
+  cin>>k;
+  int lcm=lcmMod();
+  int ans=((lcm%mod)*(k%mod))%mod;
+  cout<<ans%mod<<'\n';
+  return 0;
+}
+
+int mpower(int x, int y, int p)
+{
+int res = 1;
+x = x % p;
+while(y > 0){
+    if(y & 1) res = (res*x) % p;
+      y = y>>1;
+      x = (x*x) % p;
     }
-    return 0;
+    return res;
 }
